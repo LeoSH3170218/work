e86f384ad9cc5cce4d691ac0128342a7ea98ba7d
LeoSH3170218 <78508653+LeoSH3170218@users.noreply.github.com>
2021-08-05 10:26:22 +0300

    delete ListTile, decrease number of rows

--- lib/pages/userScreen.dart
+++ lib/pages/userScreen.dart
@@ -11,13 +11,7 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreenState extends State<UserScreen> {
-  String stringConcatenation(String firstString, String secondString) {
-    return firstString + ": " + secondString;
-  }
-  TextStyle textStyle = TextStyle(
-    fontSize: 16,
-    height: 1.8
-  );
+
 
   @override
   void initState() {
@@ -40,81 +34,53 @@ class _UserScreenState extends State<UserScreen> {
               return CircularProgressIndicator();
             } else {
               if (snapShot.data != null) {
-                AsyncSnapshot<Object?> x = snapShot;
                 return ListView.builder(itemBuilder: (_, index) {
+                  var data = (snapShot.data as dynamic)['users'][index];
                   return Container(
                     decoration: BoxDecoration(
                       border: Border.all(color: Colors.grey),
                     ),
                     padding: EdgeInsets.only(bottom: 10),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage:
-                            NetworkImage("https://ribsandburgers.com"
-                                "/au/wp-content/uploads/2019/03/"
-                                "2-angus-bacon-and-cheese.jpg"),
-                      ),
-                      title: Container(
-                        child: Row(
+                    child: Column(
+                      children: [
+                        Row(
                           children: [
-                            Text(
-                              ((snapShot.data as dynamic)['users'][index]
-                                      ['first_name']) +
-                                  " " +
-                                  (snapShot.data as dynamic)['users'][index]
-                                      ['last_name'],
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20),
-                            ),
-                          ],
-                        ),
-                      ),
-                      subtitle: Container(
-                        child: Column(
-                          children: [
-                            Row(
+                            Column(
                               children: [
-                                Text(stringConcatenation(
-                                    "email",
-                                    (snapShot.data as dynamic)['users'][index]
-                                        ['email']),
-                                  style: textStyle
+                                CircleAvatar(
+                                  backgroundImage:
+                                      NetworkImage("https://ribsandburgers.com"
+                                          "/au/wp-content/uploads/2019/03/"
+                                          "2-angus-bacon-and-cheese.jpg"),
                                 ),
                               ],
                             ),
-                            Row(
-                              children: [
-                                Text(stringConcatenation(
-                                    "title",
-                                    (snapShot.data as dynamic)['users'][index]
-                                        ['title']),
-                                  style: textStyle
-                                ),
-                              ],
+                            SizedBox(
+                              width: 30,
                             ),
-                            Row(
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(stringConcatenation(
-                                    "Job Type",
-                                    (snapShot.data as dynamic)['users'][index]
-                                        ['job_type']),
-                                  style: textStyle
+                                Text(
+                                  "Name: ${data['first_name']} "
+                                  "${data['last_name']}",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20),
                                 ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Text(stringConcatenation(
-                                    "mobile",
-                                    (snapShot.data as dynamic)['users'][index]
-                                        ['mobile']),
-                                  style: textStyle
-                                ),
+                                Text('email: ${data["email"]}',
+                                    style: textStyle),
+                                Text("title: ${data['title']}",
+                                    style: textStyle),
+                                Text("Job Type: ${data['job_type']}",
+                                    style: textStyle),
+                                Text("mobile: ${data['mobile']}",
+                                    style: textStyle),
                               ],
                             ),
                           ],
                         ),
-                      ),
+                      ],
                     ),
                   );
                 });
@@ -140,4 +106,9 @@ class _UserScreenState extends State<UserScreen> {
       return users;
     }
   }
+
+  TextStyle textStyle = TextStyle(
+    fontSize: 16,
+    height: 1.8,
+  );
 }
